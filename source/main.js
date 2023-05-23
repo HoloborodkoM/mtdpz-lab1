594f6161a877c729904ed09c506e535b29680209
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-05-23 21:35:22 +0300

    interactive: try to input data without storage

--- source/main.js
+++ source/main.js
@@ -3,24 +3,47 @@
 console.log("Which mode do you wanna use?");
 process.stdout.write("Interactive or non-interactive (press 1|2 respectively): ");
 
+let index = 0;
+
 process.stdin.on('data', checkAnswer);
 
 function checkAnswer(answer) {
 
    const enteringValue = answer.toString().trim();
+
    if (enteringValue === '1') {
-      interactive();
+      interactive(enteringValue);
    } else if(enteringValue === '2') {
       nonInteractive();
    } else {
+
       console.log("Invalid input!!! Try again.");
       process.stdout.write("Interactive or non-interactive (press 1|2 respectively): ");
+
    }
 
 }
 
-const interactive = () => {
-   process.exit();
+const interactive = (value) => {
+     
+   const enteredParameters = {};
+   const parameters = ['a', 'b', 'c'];
+
+   function parametersInitialization() {
+      process.stdout.write(`${parameters[index]} = `);
+      index++;
+   }
+   
+   enteredParameters[parameters[index]] = value;
+   console.log(enteredParameters);
+
+   if (parameters.length > index) {
+      parametersInitialization();
+   } else {
+      console.log("Done:", enteredParameters);
+      process.exit();
+   }
+     
 }
 
 const nonInteractive = () => {
